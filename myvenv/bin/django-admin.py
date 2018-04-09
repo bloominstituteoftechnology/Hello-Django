4e66223af9aa8c9773f659a3f73b5f6bbaad565f
JASPINDER SING <jaspindersingh83@gmail.com>
2018-04-09 16:05:48 -0600

    Completed Stretch Goals

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Jaspinder/Desktop/LambdaSchool/Hello-Django/myvenv/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
