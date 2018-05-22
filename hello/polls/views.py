e2e595c29165533306e154d91720da88aae47f3c
Dixie Korley <dixiekorley@gmail.com>
2018-05-22 17:01:26 -0400

    Output displaying

--- hello/polls/views.py
+++ hello/polls/views.py
@@ -11,9 +11,6 @@ def index(request):
     }
     return HttpResponse(template.render(context, request))
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 
