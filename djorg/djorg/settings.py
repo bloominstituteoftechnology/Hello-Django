cae2e45701074d5b3ca5fbda355232f8ceaad48c
Unknown <jonathanbry@comcast.net>
2018-05-29 14:45:27 -0700

    repurposing Hello-Django for the new project
    cant get ./manage.py runserver to work

--- djorg/djorg/settings.py
+++ djorg/djorg/settings.py
@@ -30,6 +30,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'bookmarks',
+    'notes',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
