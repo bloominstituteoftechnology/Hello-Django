32e9fd59bfe97f53e255599a4d04dea838dcdd23
Moises Dobarganes <designerexpert@gmail.com>
2018-07-30 18:53:15 -0400

    Began implementing config and env

--- notes/models.py
+++ notes/models.py
@@ -5,6 +5,7 @@ class User(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False) # Unique ID
     username = models.CharField(max_length=200, unique=True) # one string no bigger than 200 chars
     password = models.CharField(max_length=200) # one string no bigger than 200 chars
+    created_at = models.DateTimeField(auto_now_add=True) # like Date.now() automatically calculated on creation
 
 class Note(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False) # Unique ID
@@ -13,5 +14,3 @@ class Note(models.Model):
     created_at = models.DateTimeField(auto_now_add=True) # like Date.now() automatically calculated on creation
     last_modified = models.DateTimeField(auto_now=True) # When it was last edited auto calculater
     author = models.ForeignKey(User, on_delete=models.CASCADE) # If the Refference gets deleted, it will clear the author field.
-
-# Create your models here.
