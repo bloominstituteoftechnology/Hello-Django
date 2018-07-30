99a06a91ecbc7ebacf18b22cffa73db471043684
Justin M <jekm321@gmail.com>
2018-07-30 18:08:29 -0400

    finished re-do of all djorg tasks

--- notes/models.py
+++ notes/models.py
@@ -3,9 +3,9 @@ from uuid import uuid4
 
 # Create your models here.
 
-class Notes(models.Model):
-    id = models.UUIDField()
-    title = models.CharField()
-    content = models.TextField()
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
+class Note(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4)
+    title = models.CharField(max_length=200)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
