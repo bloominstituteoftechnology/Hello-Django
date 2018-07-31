285a23855f3a55cbc697363921e3340d0e2f8bb1
Jared Cuffe <jcuffe@gmail.com>
2018-07-30 17:31:18 -0700

    Modified Note model to bring in line with spec from lecture

--- hello/notes/models.py
+++ hello/notes/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from uuid import uuid4
 
 # Create your models here.
 class Note(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=100)
     content = models.TextField()
-    pub_date = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+    
