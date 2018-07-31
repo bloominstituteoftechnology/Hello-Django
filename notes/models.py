04012dce231d03c503c41092da14673734b446fb
hilal aissani <hilalaissani@gmail.com>
2018-07-31 00:06:23 -0400

    done

---
+++ notes/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from uuid import uuid4
+class Note(models.Model):
+    id = models.UUIDField(primary_key =True,default=uuid4,editable=False)
+    title = models.CharField(max_length= 200)
+    content = models.TextField(blank= 200)
+    created_at = models.DateTimeField(auto_now_add= 200)
+    last_modified = models.DateTimeField(auto_now= 200)
