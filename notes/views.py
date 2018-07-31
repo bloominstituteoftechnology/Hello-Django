327a0d38977d50529a008fa527f140cf83877ac6
Moises Dobarganes <designerexpert@gmail.com>
2018-07-31 19:02:01 -0400

    Notes and Users and PersonalNotes

--- notes/views.py
+++ notes/views.py
@@ -1,9 +1,15 @@
 # from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
+from notes.models import PersonalNote
 
 # Create your views here.
-def index(request):
-    template = loader.get_template('test.html')
-    context = {'test_content': [{'content': 'STUFF'},{'content': 'MORE STUFF'}]}
+def notes(request):
+    notes_list = PersonalNote.objects.all()
+    template = loader.get_template('notes.html')
+    context = {
+        'notes': notes_list
+        }
     return HttpResponse(template.render(context,request))
+
+
