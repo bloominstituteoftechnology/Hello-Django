eeaadac85be727f6aa8d1c64456c02e8686113a3
Cole Phillips <nphillips78@gmail.com>
2018-07-31 19:45:54 -0400

    I could play in tables all day long

--- notes/models.py
+++ notes/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from uuid import uuid4
+from django.contrib.auth.models import User
 # Create your models here.
 class Note(models.Model):
   id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
@@ -8,4 +9,11 @@ class Note(models.Model):
   created_at = models.DateTimeField(auto_now_add=True)
   last_modified = models.DateTimeField(auto_now=True)
 
+class PersonalNote(Note): #I love inheritance with python!
+  #this imports django's built in user class model
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+ #foreign key creates reference to data on another table
+ #cascade helps with integrity of data TODO: look this up
+
+ 
 
