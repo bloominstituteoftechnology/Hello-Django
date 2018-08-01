d9370cdc292ad9e806b8d39e7b8bbc2f38b9f1ca
JustinSHong <jhong2009@gmail.com>
2018-08-01 15:26:02 -0700

    api.py: set up PersonalNoteViewSet class

--- notes/api.py
+++ notes/api.py
@@ -1,4 +1,4 @@
-from rest_framework import serializers, viewset
+from rest_framework import serializers, viewsets
 from .models import PersonalNote
 
 class PersonalNoteSerializer(serializers.HyperlinkedModelSerializer):
@@ -6,3 +6,7 @@ class PersonalNoteSerializer(serializers.HyperlinkedModelSerializer):
 	class Meta:
 		model = PersonalNote
 		fields = ('title', 'content')
+
+class PersonalNoteViewSet(viewsets.ModelViewSet):
+	serializer_class = PersonalNoteSerializer
+	queryset = PersonalNote.objects.all()
