156d3c75095be5766625efafaed3315645044d3c
Eric Andrade <eric.andrade@gmail.com>
2018-07-31 22:08:38 -0400

    models adjustments

--- notes/models.py
+++ notes/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from uuid import uuid4
+from django.contrib.auth.models import User
 class Note(models.Model):
   id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
   title = models.CharField(max_length=200)
@@ -7,3 +8,6 @@ class Note(models.Model):
   created_at = models.DateTimeField(auto_now_add=True)
   last_modified = models.DateTimeField(auto_now=True)
   url = models.URLField(blank=True)
+
+class PersonalNote(Note):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
