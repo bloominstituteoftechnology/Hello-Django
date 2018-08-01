34d0ff31cd369d1841c09f6eb996ee83dda0f2e2
cgoez <goez2001@gmail.com>
2018-08-01 00:12:45 -0700

    Comparison, formatting

--- notes/admin.py
+++ notes/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 from .models import Note, PersonalNote
 
 # Register your models here.
-admin.site.register((Note, PersonalNote))
+admin.site.register(Note)
+admin.site.register(PersonalNote)
