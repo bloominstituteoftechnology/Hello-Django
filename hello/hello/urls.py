441d85ddd8cab8980706958c12399e20eaea2ab6
Jared Cuffe <jcuffe@gmail.com>
2018-08-01 15:48:11 -0700

    rest_framework implementation

--- hello/hello/urls.py
+++ hello/hello/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework import routers
+from notes.api import NoteViewSet
+
+router = routers.DefaultRouter()
+router.register('notes', NoteViewSet)
 
 urlpatterns = [
+    path('api/', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
