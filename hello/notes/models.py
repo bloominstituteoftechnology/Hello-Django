7fec273f9b2f4f391489525026d529807ecd357f
Jared Cuffe <jcuffe@gmail.com>
2018-07-31 23:22:42 -0700

    Invented the concept of ownership viz. Notes

--- hello/notes/models.py
+++ hello/notes/models.py
@@ -1,11 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 from uuid import uuid4
 
 # Create your models here.
 class Note(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
-    
+
+    def __str__(self):
+        return f"{self.title} - {self.user.username}"
