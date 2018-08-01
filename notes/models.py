2e1ec557fc265a2fcbabe2f0538c44ff00bf0572
totsuya <supertatortots@gmail.com>
2018-08-01 18:40:59 -0400

    added note stuff

--- notes/models.py
+++ notes/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from uuid import uuid4
+from django.contrib.auth.models import User
 
 class Note(models.Model):
   id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
@@ -8,3 +9,5 @@ class Note(models.Model):
   created_at = models.DateTimeField(auto_now_add=True)
   last_modified = models.DateTimeField(auto_now=True)
 
+class PesonalNote(Note): 
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
