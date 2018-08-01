54a2a808d17f9efa5815a8c27e2084b7ce8c7a7f
Harrison Brock <harrison@harrisonbrock.com>
2018-08-01 13:02:31 -0400

    added restful api for personal note

--- djorg/urls.py
+++ djorg/urls.py
@@ -16,9 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from rest_framework import routers
+from notes.api import PeronalNotesViewSet
+
+router = routers.DefaultRouter()
+
+router.register('notes', PeronalNotesViewSet)
+
+
 urlpatterns = [
 
-    path('notes/', include('notes.urls')),
-    path('', include('home.urls')),
+    # path('notes/', include('notes.urls')),
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls))
 ]
