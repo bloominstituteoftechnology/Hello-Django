0aeb3531ce7e0e67c2acf76ce9df10ce5a80ecc8
Harrison Brock <harrison@harrisonbrock.com>
2018-08-01 14:13:50 -0400

    fixed typo

--- djorg/urls.py
+++ djorg/urls.py
@@ -17,11 +17,11 @@ from django.contrib import admin
 from django.urls import include, path
 
 from rest_framework import routers
-from notes.api import PeronalNotesViewSet
+from notes.api import PersonalNotesViewSet
 
 router = routers.DefaultRouter()
 
-router.register('notes', PeronalNotesViewSet)
+router.register('notes', PersonalNotesViewSet)
 
 
 urlpatterns = [
