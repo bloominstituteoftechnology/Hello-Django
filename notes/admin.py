40e75a9f0213432192e72dd4e0f8ea98ca9d8760
Velsu <riftmage50@gmail.com>
2018-08-02 17:03:08 +0200

    Add REST

--- notes/admin.py
+++ notes/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question, Note
+from .models import Question, Note, PersonalNote
 
 admin.site.register(Question)
 admin.site.register(Note)
+admin.site.register(PersonalNote)
