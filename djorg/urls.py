1b922f5c2ce88263b3c4eb5684a0a6307d427657
hilal aissani <hilalaissani@gmail.com>
2018-08-01 23:33:41 -0400

    Api

--- djorg/urls.py
+++ djorg/urls.py
@@ -1,9 +1,14 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers 
+from notes.api import PersonalNoteViewSet
 
 
-
+router = routers.DefaultRouter()
+router.register('notes', PersonalNoteViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls))
+
 ]
