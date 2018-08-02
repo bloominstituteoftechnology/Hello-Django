ee3cb8564bf020abbb274396880476962e9d7dfe
ajf412 <aaronjfaulkner@yahoo.com>
2018-08-02 19:44:25 -0400

    Windows reinstall.  Error correcting complete.

---
+++ notes/api.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers, viewsets
+from .models import PersonalNote
+
+class PersonalNoteSerializer(serializers.HyperlinkedModelSerializer):
+    """Describe the model and fields we want to use."""
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+        note = PersonalNote.objects.create(**validated_data)
+        return note
+
+    class Meta:
+        model = PersonalNote
+        fields = ('title', 'content', 'url')
+
+class PersonalNoteViewSet(viewsets.ModelViewSet):
+    """Describe the rows we want from the DB."""
+
+    serializer_class = PersonalNoteSerializer
+    queryset = PersonalNote.objects.all()
+  
