9fa049fb2b55eeaa39a5dd7306a8a3fbc5380896
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-08-02 15:36:30 -0400

    Fixed typo, authtokens to authtoken

--- djorg/settings.py
+++ djorg/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authtokens'
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
