ce9d3ae93596518d1a4d7cd1ce32d2b13c8afba4
David Alexander Morales <damorales89@gmail.com>
2018-08-02 05:40:07 -0400

    rest_framework added

--- djorg/settings.py
+++ djorg/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'djorg.notes'
+    'djorg.notes',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# DJANGO REST BOILERPLATE
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    ]
+}
