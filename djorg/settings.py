582386096ddd170d9b99fa91979d1d60d0865936
ajf412 <aaronjfaulkner@yahoo.com>
2018-08-03 15:45:05 -0400

    Lecture corrections take 1

--- djorg/settings.py
+++ djorg/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
@@ -87,6 +87,10 @@ WSGI_APPLICATION = 'djorg.wsgi.application'
 #     }
 # }
 
+DATABASES = {}
+
+DATABASES['default'] = dj.database_url.config('DATABASE_URL')
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
@@ -125,6 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 
