4b3dd9079523a338fb4f8e6cad2d3196a905bb17
Cruise Brantley <cruise.brantley@gmail.com>
2018-08-03 13:14:00 -0500

    fix

--- djorg/settings.py
+++ djorg/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-DATABASES = []
+DATABASES = {}
 DATABASES['default'] = dj_database_url.config('DATABASE_URL')
 
 ALLOWED_HOSTS = ['*']
