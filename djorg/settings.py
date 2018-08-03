262cdd0d173709a42eb79ba8d4e4fd5043548304
Mark Wong <fmarkwong@gmail.com>
2018-08-03 12:37:26 -0700

    stuff

--- djorg/settings.py
+++ djorg/settings.py
@@ -141,6 +141,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+# https://stackoverflow.com/questions/50866421/500-error-in-heroku-when-debug-false
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 # Activate Django-Heroku.
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
