00057ffd4322ff9bf96e10f37308f5298310a7ff
Shawn Stewart <s.stewart0220@gmail.com>
2018-08-03 14:38:52 -0400

    Making migrations

--- DjangoIntro/settings.py
+++ DjangoIntro/settings.py
@@ -83,7 +83,9 @@ WSGI_APPLICATION = 'DjangoIntro.wsgi.application'
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES = {
+    'default': dj_database_url.config('sqlite:///db.sqlite3', conn_max_age=600)
+}
 
 
 # Password validation
