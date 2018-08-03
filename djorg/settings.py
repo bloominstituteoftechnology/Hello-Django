07d063861127a2b765025dd3957d785c239328be
JustinSHong <jhong2009@gmail.com>
2018-08-03 09:42:43 -0700

    reconfig databse url

--- djorg/settings.py
+++ djorg/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
 }
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
-DATABASES['default'] = dj_database_url.config(DATABASE_URL)
+DATABASES['default'] = dj_database_url.config('DATABASE_URL')
 
 
 # Password validation
