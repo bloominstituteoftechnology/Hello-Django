e42c92bd32875531b2d19e5ff9512444234b7bb0
David Alexander Morales <damorales89@gmail.com>
2018-08-03 17:19:40 -0400

    database fix?

--- djorg/settings.py
+++ djorg/settings.py
@@ -91,14 +91,17 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 
-DATABASES = {}
+DATABASE_URL = config('DATABASE_URL')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# DATABASES = {}
 DATABASES['default'] = dj_database_url.config('DATABASE_URL')
 
 # Password validation
