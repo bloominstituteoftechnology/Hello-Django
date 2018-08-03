241518e01640f4c2c42e5cc0187b1699f5a76fb9
Velsu <riftmage50@gmail.com>
2018-08-03 17:11:19 +0200

    default database and allowed hosts

--- djorg/settings.py
+++ djorg/settings.py
@@ -15,6 +15,9 @@ import os
 from decouple import config
 import dj_database_url
 
+DATABASES['default'] =  dj_database_url.config()
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -28,7 +31,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
