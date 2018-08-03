a989fe64ab12567f9a7d9938104f402748ee4013
Yasin Shuman <email@yasin.io>
2018-08-03 13:05:14 -0700

    fixing db

--- djorg/settings.py
+++ djorg/settings.py
@@ -79,9 +79,9 @@ WSGI_APPLICATION = 'djorg.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(default=config('DATABASE_URL'))
-}
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'))
+
 
 
 # DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
