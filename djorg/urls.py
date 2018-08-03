bd55488ac17addb022d2b28033e55fb82d522048
Cole Phillips <nphillips78@gmail.com>
2018-08-03 14:09:56 -0400

    added required user field

--- djorg/urls.py
+++ djorg/urls.py
@@ -14,9 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
+from rest_framework import routers
+from notes.api import PersonalNoteViewSet
+
+
+router = routers.DefaultRouter()
+router.register('notes', PersonalNoteViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    #re_path('^api-token-auth/', views.obtain_auth_token)
 ]
