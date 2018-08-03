3663bc66f9625e0f17a2131d296d233072760ec2
cgoez <goez2001@gmail.com>
2018-08-03 10:16:03 -0700

    Add ALLOWED_HOSTS

--- djorg/settings.py
+++ djorg/settings.py
@@ -31,7 +31,9 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".herokuapp.com"
+]
 
 
 # Application definition
