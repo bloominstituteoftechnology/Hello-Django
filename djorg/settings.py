6a52e911f4a58d6a0e208dc3fa64ba7312133964
Mark Wong <fmarkwong@gmail.com>
2018-08-03 11:37:31 -0700

    pipfile

--- djorg/settings.py
+++ djorg/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['*']
 
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 
