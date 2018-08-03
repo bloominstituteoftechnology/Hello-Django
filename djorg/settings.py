5d8101144bfb209f43f831c8ea40411f3b1b300d
Eric Andrade <eric.andrade@gmail.com>
2018-08-03 11:16:57 -0400

    configured dotenv file, generated secret key

--- djorg/settings.py
+++ djorg/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2deg^-l@z%1p10q4vwex-c!sl14tdt2+7ips=f8lm2&-5i@7mh'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
