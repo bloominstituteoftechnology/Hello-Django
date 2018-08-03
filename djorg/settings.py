4442b9031e8b075dc588ac43b87c41eacca73164
David Alexander Morales <damorales89@gmail.com>
2018-08-03 16:06:59 -0400

    more slight changes

--- djorg/settings.py
+++ djorg/settings.py
@@ -92,13 +92,13 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
-DATABASES['default'] = dj_database_url.config(default="sqlite:///db.sqlite3")
+DATABASES['default'] = dj_database_url.config('DATABASE_URL')
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
