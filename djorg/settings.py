2fc533e7aeffaf3b2f72c7875409b9539d514eaf
Mark Wong <fmarkwong@gmail.com>
2018-08-03 12:17:18 -0700

    settings

--- djorg/settings.py
+++ djorg/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 import dj_database_url
 from decouple import config
 
@@ -140,3 +141,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+# Activate Django-Heroku.
+django_heroku.settings(locals())
