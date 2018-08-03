99ab6288f2a0f4dc44ce37cda57c1a8589f88ded
Shawn Stewart <s.stewart0220@gmail.com>
2018-08-03 13:39:13 -0400

    Making migrations

--- DjangoIntro/settings.py
+++ DjangoIntro/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = herokuapp.com
+ALLOWED_HOSTS = 'herokuapp.com'
 
 
 # Application definition
