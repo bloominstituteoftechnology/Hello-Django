66c99fadeb41a99e2515e9a71c9932b373132e26
Shawn Stewart <s.stewart0220@gmail.com>
2018-08-03 13:06:04 -0400

    Deploying to heroku

--- DjangoIntro/settings.py
+++ DjangoIntro/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = .herokuapp.com
 
 
 # Application definition
@@ -76,12 +77,13 @@ WSGI_APPLICATION = 'DjangoIntro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 # Password validation
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn DjangoIntro.wsgi
