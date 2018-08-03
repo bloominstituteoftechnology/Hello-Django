5ee8f72d8218384b4d3363f5785b845fe538293f
Natalie <natalieschennum@gmail.com>
2018-08-03 10:11:53 -0700

    Sprint Stuff

--- djorg/settings.py
+++ djorg/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,7 +27,11 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','127.0.0.1', 'myfirstdjangoproject1.herokuapp.com']
+DATABASE_URL = config('DATABASE_URL')
+DATABASES['default'] = dj_database_url.parse('postgres://...', conn_max_age=600)
+
+
 
 
 # Application definition
@@ -41,6 +46,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework', 
     'rest_framework.authtoken', 
+]
+
+MIDDLEWARE_CLASSES = [
+  # 'django.middleware.security.SecurityMiddleware',
+  'whitenoise.middleware.WhiteNoiseMiddleware',
+  # ...
 ]
 
 MIDDLEWARE = [
