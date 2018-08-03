1357e752e89f91e35fabda79a029d0e41df9884b
Jared Cuffe <jcuffe@gmail.com>
2018-08-03 11:47:43 -0700

    static files changes to appease django

--- hello/hello/settings.py
+++ hello/hello/settings.py
@@ -18,7 +18,6 @@ import whitenoise
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
@@ -119,9 +118,14 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 
