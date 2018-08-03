e9b6b8cfbeea6c129e772f25a026882e0199cf48
ajf412 <aaronjfaulkner@yahoo.com>
2018-08-03 15:46:23 -0400

    Fixed dj_database_url.config

--- djorg/settings.py
+++ djorg/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'djorg.wsgi.application'
 
 DATABASES = {}
 
-DATABASES['default'] = dj.database_url.config('DATABASE_URL')
+DATABASES['default'] = dj_database_url.config('DATABASE_URL')
 
 
 # Password validation
