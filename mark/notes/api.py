f67da7675f139e6d899518c4f99f0bc55b8dbc0f
markstez05 <36493764+markstez05@users.noreply.github.com>
2018-08-03 12:01:58 -0500

    created endpoints/user specific views

--- mark/notes/api.py
+++ mark/notes/api.py
@@ -3,12 +3,26 @@ from .models import PersonalNote
 
 class PersonalNoteSerializer(serializers.HyperlinkedModelSerializer):
     """DESCRIBE THE MODEL AND FIELDS WE WANT TO USE"""
+
+    def create(self, validated_data):
+        #import pdb; pdb.set_trace()
+        user = self.context['request'].user
+        note = PersonalNote.objects.create(user=user, **validated_data)
+        return note
+
     class Meta:
         model = PersonalNote
-        fields = ('title', 'content')
+        fields = ('title', 'content', 'url')
 
 class PersonalNoteViewSet(viewsets.ModelViewSet):
     """Describe the rows we want from the DB"""
 
     serializer_class = PersonalNoteSerializer
-    queryset = PersonalNote.objects.all()
+    queryset = PersonalNote.objects.none()
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_anonymous:
+            return PersonalNote.objects.none()
+        else:
+            return PersonalNote.objects.filter(user=user)
