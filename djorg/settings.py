a7059f5f2a4f9a276aec5d6e0194cf4f4e1219a7
Yasin Shuman <email@yasin.io>
2018-08-03 11:17:10 -0700

    playing with procfile

--- djorg/settings.py
+++ djorg/settings.py
@@ -81,9 +81,11 @@ WSGI_APPLICATION = 'djorg.wsgi.application'
 
 DATABASES = { 
     'default': dj_database_url.config(
-        default=config('DATABASE_URL')
+        default=config(db_from_env)
     )
 }
+db_from_env = dj_database_url.config(conn_max_age=500, require_ssl=True)
+DATABASES['default'].update(db_from_env)
 
 
 # DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
