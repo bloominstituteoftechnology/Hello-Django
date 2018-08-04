6574a0cf3ed076e4c1fd2f88c5aa6132e81c98db
Chris <cjmarsh725@gmail.com>
2018-08-04 12:07:25 -0700

    Added token authorization (Day 4)

--- djorg/settings.py
+++ djorg/settings.py
@@ -40,12 +40,20 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'notes',
     'rest_framework',
+    'rest_framework.authtoken',
 ]
+
+from rest_framework.authentication import SessionAuthentication, BaseAuthentication, TokenAuthentication
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ),
 }
 
 MIDDLEWARE = [
