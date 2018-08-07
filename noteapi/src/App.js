24a7b8da4d8efb1065523ac124c3c4f7421c4bce
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-08-07 19:19:43 -0400

    Able to request note information by sending token

--- noteapi/src/App.js
+++ noteapi/src/App.js
@@ -4,21 +4,42 @@ import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+    }
+  }
 
   componentDidMount() {
-    axios.get("https://frozen-ridge-71012.herokuapp.com/api/notes")
-    .then(res => {
-      console.log(res);
-    })
-    .catch(err => {
-      console.log(err);
-    })
-  }
+  
+    const token = "Token a30df5495e88a321b9d83f4b435f65502a0ebc0c"
+
+    const optionTwo = {
+        method: 'GET',
+        headers: { 'content-type': 'application/json', 'Authorization': token },
+        url: "https://frozen-ridge-71012.herokuapp.com/api/notes/",
+    }
+
+    axios(optionTwo)
+        .then(res => {
+            console.log(res)
+            this.setState({ data: res.data })
+        })
+        .catch(err => {
+          console.log(err)
+        });
+}
 
   render() {
     return (
       <div className="App">
-        
+        <div>{ this.state.data.map(note => {
+          return <div> (
+            Title: { note.title } Content: { note.content }
+          )
+            </div>
+        }) }</div>
       </div>
     );
   }
