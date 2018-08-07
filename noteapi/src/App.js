e92de7ef51db29b49e6acb62ef755b639472f4b3
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-08-07 18:12:59 -0400

    Added axios

--- noteapi/src/App.js
+++ noteapi/src/App.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+    axios.get("https://frozen-ridge-71012.herokuapp.com/api/notes")
+    .then(res => {
+      console.log(res);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+
   render() {
     return (
       <div className="App">
