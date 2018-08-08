f59923ba4542d6ea339e713c6955e6eff85474bd
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-08-08 16:44:41 -0400

    Implemented graphene_django

--- djorg/settings.py
+++ djorg/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'corsheaders'
+    'corsheaders',
+    'graphene_django'
 ]
 
 MIDDLEWARE = [
