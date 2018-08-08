787776afdd804f4bf451da0c56cac316cece1bf6
Mark Marchant <jtla3@yahoo.com>
2018-08-08 15:02:26 -0700

    remove SECRET_KEY from setting and added to env file

--- djorg/settings.py
+++ djorg/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ru&_e#2kfm%-2iz%2mzn)5^97=q1t+oex=36z!d-1ua4%nmrr7'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
