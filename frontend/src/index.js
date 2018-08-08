ce052a2df1793f65b0b24fd3d543125474a69c10
JacquelynnRohrich <33401573+JacquelynnRohrich@users.noreply.github.com>
2018-08-08 18:52:21 -0500

    troubleshooting apollo still but implemented axios for now.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,26 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import ApolloClient from 'apollo-boost';
-import gql from 'graphql-tag';
+// import ApolloClient from 'apollo-boost';
+import axios from 'axios';
 
-const client = new ApolloClient ({
-    uri: "http://localhost:8000/graphql/"
-});
-
-client
-    .query({
-        query: gql` {
-                notes {
-                id 
-                title
-                content
-            }
-        }
-        `
-    })
-
-.then(result => console.log(result));
-
+axios.get('http://localhost:8000/graphql/?query=%7B%0A%20%20notes%20%7B%0A%20%20%20%20id%0A%20%20%20%20title%0A%20%20%20%20content%0A%20%20%7D%0A%7D%0A&operationName=null')
+.then (response => console.log(response.data.data.notes));
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+// import gql from 'graphql-tag';
+
+// const client = new ApolloClient ({
+//     uri: "http://localhost:8000/graphql/"
+// });
+
+// client
+//     .query({
+//         query: gql` {
+//                 notes {
+//                 id 
+//                 title
+//                 content
+//             }
+//         }
+//         `
+//     })
+
+// .then(result => console.log(result));
