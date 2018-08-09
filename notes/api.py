5bca615cf14c0bde641c83db443876056c047bf9
Mark Marchant <jtla3@yahoo.com>
2018-08-09 02:31:17 -0700

    overwrite the create method in PersonalNoteSerializer class

--- notes/api.py
+++ notes/api.py
@@ -4,6 +4,10 @@ from .models import PersonalNote
 class PersonalNoteSerializer(serializers.HyperlinkedModelSerializer):
     """Describe the model and fields we want to use."""
 
+    def create(self, validated_data):
+        note = PersonalNote.object.create(**validated - data)
+        return note
+
     class Meta:
         model = PersonalNote
         fields = ('title', 'content', 'url')
